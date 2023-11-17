6d9f2200bea77d4e6ee7d6ee794ead1678d986d1
Thales Vieira Pereira <thalesvpereira017@gmail.com>
2023-11-17 00:33:29 -0300

    adção classe Aula
    sem documentação

--- src/models/Aula.java
+++ src/models/Aula.java
@@ -1,12 +1,51 @@
 package models;
 
 import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Aula {
-    private int idAulas;
-    private int fk_idProfessores;
+    private int idAula;
+    private Professor Professor;
     private String nome;
     private Time horarioInicio;
     private Time horarioFim;
     private String diaSemana;
+    private ArrayList<Aluno> alunos;
+
+
+    public Aula(models.Professor professor, String nome, Time horarioInicio, Time horarioFim, String diaSemana){
+        Professor = professor;
+        this.nome = nome;
+        this.horarioInicio = horarioInicio;
+        this.horarioFim = horarioFim;
+        this.diaSemana = diaSemana;
+        this.alunos = new ArrayList<>();
+    }
+
+
+    
+
+
+    public Aula(int idAula, Professor professor, String nome, Time horarioInicio, Time horarioFim, String diaSemana) {
+        this.idAula = idAula;
+        Professor = professor;
+        this.nome = nome;
+        this.horarioInicio = horarioInicio;
+        this.horarioFim = horarioFim;
+        this.diaSemana = diaSemana;
+        this.alunos = new ArrayList<>();
+    }
+
+
+    public void addAluno(Aluno aluno) {
+        if (aluno != null) {
+            alunos.add(aluno);
+        }
+    }
+
+
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
 }
