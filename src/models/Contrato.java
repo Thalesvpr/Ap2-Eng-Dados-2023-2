e9b22bfbf9aec728838eb6136df4a590becc1f6a
ivolavacek <ivo.lavacek@gmail.com>
2023-11-17 00:33:18 -0300

    models aluno e contrato

--- src/models/Contrato.java
+++ src/models/Contrato.java
@@ -1,15 +1,99 @@
 package models;
 
-import java.sql.Date;
+import java.time.LocalDate;
 
 public class Contrato {
-    private float idContrato;
-    private Date dataInicio;
-    private Date dataTermino;
-    private float valorMensalidade;
+
+    private int idContrato;
+    private LocalDate dataInicio;
+    private LocalDate dataTermino;
+    private float valorMensalidade = 0;
     private String tipo;
-    private int qtdAulas;
-    private float valorPorAula;
+    private int qtdAulas = 0;
+    private float valorPorAula = 0;
 
 
+    public Contrato(int idContrato, LocalDate dataInicio, LocalDate dataTermino, String tipo, int qtdAulas, float valorPorAula) {
+        this.dataInicio = dataInicio;
+        this.dataTermino = dataTermino;
+        this.tipo = tipo;
+        this.qtdAulas = qtdAulas;
+        this.valorPorAula = valorPorAula;
+        this.valorMensalidade = calculaMensalidade(this.qtdAulas, this.valorPorAula);
+    }
+
+    public Contrato(LocalDate dataInicio, LocalDate dataTermino, String tipo, int qtdAulas, float valorPorAula) {
+        this.dataInicio = dataInicio;
+        this.dataTermino = dataTermino;
+        this.tipo = tipo;
+        this.qtdAulas = qtdAulas;
+        this.valorPorAula = valorPorAula;
+        this.valorMensalidade = calculaMensalidade(this.qtdAulas, this.valorPorAula);
+    }
+
+    public Contrato(LocalDate dataInicio, LocalDate dataTermino, String tipo) {
+        this.dataInicio = dataInicio;
+        this.dataTermino = dataTermino;
+        this.tipo = tipo;
+    }
+
+    private float calculaMensalidade(int qtdAulas, float valorPorAula) {
+        return qtdAulas * valorPorAula;
+    }
+
+    public int getIdContrato() {
+        return idContrato;
+    }
+
+    public void setIdContrato(int idContrato) {
+        this.idContrato = idContrato;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataTermino() {
+        return dataTermino;
+    }
+
+    public void setDataTermino(LocalDate dataTermino) {
+        this.dataTermino = dataTermino;
+    }
+
+    public float getValorMensalidade() {
+        return valorMensalidade;
+    }
+
+    public void setValorMensalidade(float valorMensalidade) {
+        this.valorMensalidade = valorMensalidade;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getQtdAulas() {
+        return qtdAulas;
+    }
+
+    public void setQtdAulas(int qtdAulas) {
+        this.qtdAulas = qtdAulas;
+    }
+
+    public float getValorPorAula() {
+        return valorPorAula;
+    }
+
+    public void setValorPorAula(float valorPorAula) {
+        this.valorPorAula = valorPorAula;
+    }
 }
